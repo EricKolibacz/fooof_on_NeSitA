ab5ae8a7581e3fcc32bbab2dcf34e7f3626aa2ef
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-05-19 17:39:15 +0200

    Create function for splitting data - based on blocks

---
+++ split_in_blocks.m
@@ -0,0 +1,27 @@
+% predict_with_LDA - ZZZ
+%
+% Input:
+%   YYY             - YYY
+% 
+% Output:
+%   XXX             - XXX.
+%                  
+%
+% Example usage: XXX
+%
+% Author: Eric Kolibacz, 2021
+%
+% See also: -
+%           
+% This function is free for any kind of distribution and usage!
+% ----------------
+function split_in_blocks(EEG)
+
+    % check if inputs are given and display the help otherwise
+    % input check: if no arguments are entered, print the help and stop
+    if nargin < 100
+        help extract_numeric_and_cell_data_from_table
+        return
+    end
+
+end
