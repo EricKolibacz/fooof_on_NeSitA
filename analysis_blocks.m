30f9da205a19687a6d6914e820e26ca638bfc7a0
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-05-20 17:50:45 +0200

    Extract moving window logic and include first plotting draft

--- analysis_blocks.m
+++ analysis_blocks.m
@@ -1,3 +1,4 @@
+%% Reading data
 person = 's6';
 if ~ exist("eeg_blocks", 'var')
     eeg_blocks = load(['/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/' person '/blocks.mat']);
@@ -6,20 +7,26 @@ load(['/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/' person '
 block_names = fieldnames(eeg_blocks);
 
 aperiodic_offsets = zeros(length(block_names),1);
-for block_name_i = 1:length(block_names)
-    block_name_i
-    [psd, freqs] = pwelch(eeg_blocks.(block_names{block_name_i}).data, srate, [], [], srate);
-
-    % Transpose, to make inputs row vectors
-    freqs = freqs';
-    psd = psd';
-
-    % FOOOF settings
-    settings = struct();  % Use defaults
-    f_range = [1, 55];
-
-    % Run FOOOF
-    fooof_results = fooof(freqs, psd, f_range, settings);
-
-    aperiodic_offsets(block_name_i) = fooof_results.aperiodic_params(1);
+for block_name_i = 10:13%length(block_names)
+    block_data = eeg_blocks.(block_names{block_name_i}).data;
+    
+    %moving window
+    block_result = analysis_with_fooof_and_moving_window(block_data, srate, 10000, 1000);
+    block_names{block_name_i}
+    block_results.(block_names{block_name_i}) = block_result;
 end
+
+%% Plotting aperiodic parameters
+figure(1);
+for block_name_i = 10:13%length(block_names)
+    block_name = block_names{block_name_i};
+    if isempty(block_results.(block_name))
+       continue 
+    end
+    aperiodic_parameters = vertcat(block_results.(block_name).aperiodic_params)
+    plot(aperiodic_parameters(:,1), 'DisplayName', block_name);
+    
+hold on
+    title('Aperiodic offset')
+end
+legend;
