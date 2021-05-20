978e24e69a681aad5b7fcd0a79efe809d185e97b
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-05-20 17:51:21 +0200

    Create function for analysis data with moving window

---
+++ analysis_with_fooof_and_moving_window.m
@@ -0,0 +1,75 @@
+% predict_with_LDA - ZZZ
+%
+% Input:
+%   YYY             - YYY
+% 
+% Output:
+%   XXX             - XXX.
+%   window_size     - Window size is measured in milliseconds.
+%                  
+%
+% Example usage: XXX
+%
+% Author: Eric Kolibacz, 2021
+%
+% See also: -
+%           
+% This function is free for any kind of distribution and usage!
+% ----------------
+function results = analysis_with_fooof_and_moving_window(data, srate, window_size, varargin)
+
+    % check if inputs are given and display the help otherwise
+    % input check: if no arguments are entered, print the help and stop
+    if nargin < 3
+        help extract_numeric_and_cell_data_from_table
+        return
+    end
+    
+    % input parsing settings
+    p = inputParser;
+    p.CaseSensitive = false;
+    
+    dataRequirements = @(x) (isa(x, 'single')) && ~isempty(x);
+    requirements = @(x) (isa(x, 'double')) && ~isempty(x);
+        
+    addRequired(p, 'data', dataRequirements);
+    addRequired(p, 'srate', requirements);
+    addRequired(p, 'window_size', requirements);
+    addOptional(p, 'window_steps', 100 ,requirements);
+    
+    % parse the input
+    parse(p, data, srate, window_size, varargin{:});
+    
+    data = p.Results.data;
+    srate = p.Results.srate;
+    window_size = p.Results.window_size;
+    window_steps = p.Results.window_steps;   
+    if length(data) < window_size
+        warning('Window size larger than available amount of data. Skipping ...')
+        results = ([]);
+    else
+        window_index_size = srate*window_size/1000;
+
+        % results = struct([]);% zeros(floor(length(data)/window_steps)+1,1);
+
+        for data_i = 1:window_steps:length(data)
+            if data_i+window_index_size > length(data)
+               continue 
+            end
+            [psd, freqs] = pwelch(data(data_i:data_i+window_index_size), srate, [], [], srate);
+
+            % Transpose, to make inputs row vectors
+            freqs = freqs';
+            psd = psd';
+
+            % FOOOF settings
+            settings = struct();  % Use defaults
+            f_range = [1, 55];
+
+            % Run FOOOF
+            fooof_results = fooof(freqs, psd, f_range, settings);
+
+            results(floor(data_i/window_steps)+1) = fooof_results;
+        end
+    end
+end
