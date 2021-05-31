fde39f4df54760498f91b1b6684ca1219ff6130a
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-05-31 14:45:53 +0200

    Include performance - in window, amount (not events themselves) are saved - block_result includes both information now - plotting is possible

--- analysis_blocks.m
+++ analysis_blocks.m
@@ -1,5 +1,9 @@
-%% Reading data
+%% Parameters
 person = 's6';
+window_size = 30000;
+window_steps = 4000;
+
+%% Reading data
 if ~ exist("eeg_blocks", 'var')
     extracted_data = load(['/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/' person '/blocks.mat']);
 end
@@ -13,18 +17,20 @@ channels = extracted_data.channels;
 block_names = fieldnames(eeg_blocks);
 
 aperiodic_offsets = zeros(length(block_names),1);
-for block_name_i = 10:13%length(block_names)
-    block_data = eeg_blocks.(block_names{block_name_i}).data;
+for block_name_i = 13:13% (block_names)
+    block_data = eeg_blocks.(block_names{block_name_i});
     
     %moving window
-    block_result = analysis_with_fooof_and_moving_window(block_data, channels, srate, 30000, 4000);
+    block_result = analysis_with_fooof_and_moving_window(block_data, channels, srate, window_size, window_step);
     block_names{block_name_i}
     block_results.(block_names{block_name_i}) = block_result;
 end
 
+
+
 %% Plotting aperiodic parameters
 figure(1);
-for block_name_i = 10:10%length(block_names)
+for block_name_i = 13:13%length(block_names)
     block_name = block_names{block_name_i};
     if isempty(block_results.(block_name))
        continue 
@@ -33,11 +39,21 @@ for block_name_i = 10:10%length(block_names)
         channel = ['channel_' num2str(channels(i_channel))];
         all_windows_of_block = struct2cell(block_results.(block_name));
         aperiodic_parameters = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).aperiodic_params);
+        performance = (vertcat(vertcat(all_windows_of_block{:}).hits) + vertcat(vertcat(all_windows_of_block{:}).CRs)) ./ ...
+        (vertcat(vertcat(all_windows_of_block{:}).hits) + vertcat(vertcat(all_windows_of_block{:}).CRs) + vertcat(vertcat(all_windows_of_block{:}).misses) + vertcat(vertcat(all_windows_of_block{:}).FAs));
+        time = 0:size(all_windows_of_block,1)-1;
+        time = time' * window_steps/1000;
         
         subplot(2,2,i_channel);
-        plot(aperiodic_parameters(:,1), 'DisplayName', block_name);
+        hold on
+        yyaxis left
+        plot(time, aperiodic_parameters(:,1), 'DisplayName', block_name);
+        % ylim([0 5])
+        yyaxis right
+        plot(time, performance, 'DisplayName', block_name);
+        ylim([0 1])
         title(['Aperiodic offset for channel ' num2str(channels(i_channel))])
+        ylabel('Performance')
+        xlabel('Time in s')
     end
-    hold on
 end
-%legend;
--- analysis_with_fooof_and_moving_window.m
+++ analysis_with_fooof_and_moving_window.m
@@ -29,7 +29,7 @@ function results = analysis_with_fooof_and_moving_window(data, channels, srate, 
     p = inputParser;
     p.CaseSensitive = false;
     
-    dataRequirements = @(x) (isa(x, 'single')) && ~isempty(x);
+    dataRequirements = @(x) (isa(x, 'struct')) && ~isempty(x);
     requirements = @(x) (isa(x, 'double')) && ~isempty(x);
         
     addRequired(p, 'data', dataRequirements);
@@ -42,25 +42,31 @@ function results = analysis_with_fooof_and_moving_window(data, channels, srate, 
     parse(p, data, channels, srate, window_size, varargin{:});
     
     data = p.Results.data;
+    eeg_data = data.data;
+    events = data.events;
     channels = p.Results.channels;
     srate = p.Results.srate;
+    
+    start_time_index = events.latency;
     window_size = p.Results.window_size; % window size in ms
-    window_index_size = srate*window_size/1000; % window size in # data points
+    window_index_size = to_index(window_size, srate); % window size in # data points
     window_steps = p.Results.window_steps; % step size in ms
-    window_index_steps = floor(srate*window_steps/1000); % step size in # data points
+    window_index_steps = to_index(window_steps, srate); % step size in # data points
     
     results = struct;
     
-    if size(data,2) < window_index_size
-        warning(['Window size (' num2str(window_size) 'ms) larger than the data (' num2str(length(data)/srate*1000) 'ms). Skipping block...'])
+    if size(eeg_data,2) < window_index_size
+        warning(['Window size (' num2str(window_size) 'ms) larger than the data (' num2str(length(eeg_data)/srate*1000) 'ms). Skipping block...'])
     else
-        for data_i = 1:window_index_steps:size(data,2)
-            if data_i+window_index_size > size(data,2)
+        for data_i = 1:window_index_steps:size(eeg_data,2)
+            if data_i+window_index_size > size(eeg_data,2)
                continue 
             end
             window = ['window_' num2str(floor(data_i/window_index_steps)+1)];
+            
+            % analysing eeg date via fooof
             for channel_i = 1:length(channels)
-                [psd, freqs] = pwelch(data(channel_i, data_i:data_i+window_index_size), srate, [], [], srate);
+                [psd, freqs] = pwelch(eeg_data(channel_i, data_i:data_i+window_index_size), srate, [], [], srate);
 
                 % Transpose, to make inputs row vectors
                 freqs = freqs';
@@ -74,8 +80,29 @@ function results = analysis_with_fooof_and_moving_window(data, channels, srate, 
                 fooof_results = fooof(freqs, psd, f_range, settings);
 
                 channel = ['channel_' num2str(channels(channel_i))];
+                
                 results.(window).(channel) = fooof_results;
             end
+            
+            % analysing performance
+            window_start_time = start_time_index + data_i;
+            window_end_time = start_time_index + data_i + window_index_size;
+            
+            window_events = events(1,cell2mat({events.latency}') > window_start_time & cell2mat({events.latency}') < window_end_time);
+            window_destruction_events = window_events(contains({window_events.type}','sphereDestruction'));
+            window_hit_events = window_destruction_events(contains({window_destruction_events.performance}','hit'));
+            window_CR_events = window_destruction_events(contains({window_destruction_events.performance}','correctRejection'));
+            window_miss_events = window_destruction_events(contains({window_destruction_events.performance}','miss'));
+            window_FA_events = window_destruction_events(contains({window_destruction_events.performance}','falseAlarm'));
+            
+            if length(window_destruction_events)~=length(window_hit_events)+length(window_CR_events)+length(window_miss_events)+length(window_FA_events)
+                warning('Some logical error ocurred while calculating the performance...')
+            end
+            
+            results.(window).hits = length(window_hit_events);
+            results.(window).CRs = length(window_CR_events);
+            results.(window).misses = length(window_miss_events);
+            results.(window).FAs = length(window_FA_events);
         end
     end
 end
