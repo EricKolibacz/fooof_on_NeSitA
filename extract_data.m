2c97c66dd795a9f091b6db0d82292546bb023062
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-06-07 16:33:50 +0200

    Include eeglab commands + filtering command (!) - somehow data needs to be filtered - to be sure it's done we include alle the commands here

--- extract_data.m
+++ extract_data.m
@@ -1,7 +1,29 @@
+%% Parameters
 channels = [85, 87, 89, 90];
+parent_folder = '/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/';
 
+%% Which person
+persons=get_files(parent_folder, 'just_folders', true);
+[indx,tf] = listdlg('PromptString',{'Select a person.',...
+    'Only one can be selected at a time.',''},...
+    'SelectionMode','single',...
+    'ListSize',[350,300],...
+    'ListString',persons);
+if tf == 0
+   error('A person needs to be selected') 
+end
+person = persons{indx};
+
+%% Extracting
+filepath = ['/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/' person '/'];
+[ALLEEG, ~, ~, ALLCOM] = eeglab;
+EEG = pop_loadset('filename',[person '_cleaned_with_ICA.set'],'filepath', filepath);
+[ALLEEG, EEG, ~] = eeg_store( ALLEEG, EEG, 0 );
+EEG = pop_eegfiltnew(EEG, 'locutoff',1,'plotfreqz',1);
+[ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'gui','off'); 
 eeg_blocks = split_in_blocks(EEG, channels);
 extracted_data.eeg_blocks = eeg_blocks;
 extracted_data.srate = EEG.srate(1);
 extracted_data.channels = channels;
-save([EEG.filepath 'extracted_data_channels_' strjoin(arrayfun(@num2str, channels, 'Uniform', false),'_') '.mat'],'-struct', 'extracted_data')
+save([filepath 'extracted_data_channels_' strjoin(arrayfun(@num2str, channels, 'Uniform', false),'_') '.mat'],'-struct', 'extracted_data')
+disp('Done extracting ...')
