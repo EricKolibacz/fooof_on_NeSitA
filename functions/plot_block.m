ec75f996d78df067cb8899dcdeed477c2c01c933
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-06-08 14:32:54 +0200

    Restructure and improve plotting blocks - include option of choosing between component and offset - adapt yaxis label - adapt titles - improve x-axis - add comments for possible error implementation and nan inclusion -

--- functions/plot_block.m
+++ functions/plot_block.m
@@ -15,7 +15,7 @@
 %           
 % This function is free for any kind of distribution and usage!
 % ----------------
-function files = plot_block(block, channels, step_size)
+function plot_block(block, channels, step_size, window_size, varargin)
 
     % check if inputs are given and display the help otherwise
     % input check: if no arguments are entered, print the help and stop
@@ -30,17 +30,29 @@ function files = plot_block(block, channels, step_size)
     
     struct_requirements = @(x) (isa(x, 'struct')) && ~isempty(x);
     double_requirements = @(x) (isa(x, 'double')) && ~isempty(x);
+    string_requirements = @(x) (isa(x, 'char')) && ~isempty(x);
         
     addRequired(p, 'block', struct_requirements);
     addRequired(p, 'channels', double_requirements);
     addRequired(p, 'step_size', double_requirements);
+    addRequired(p, 'window_size', double_requirements);
+    addOptional(p, 'parameter', 'component', string_requirements);
     
     % parse the input
-    parse(p, block, channels, step_size);
+    parse(p, block, channels, step_size, window_size, varargin{:});
     
     block = p.Results.block;
     channels = p.Results.channels;
     step_size = p.Results.step_size;
+    window_size = p.Results.window_size;
+    parameter = p.Results.parameter;
+    if strcmp(p.Results.parameter,'component')
+        parameter_idx = 1;
+    elseif strcmp(p.Results.parameter,'offset')
+        parameter_idx = 2;
+    else
+       error(['The parameter ' p.Results.parameter ' if not know']) 
+    end
     
     % plotting
     for i_channel = 1:length(channels)
@@ -50,22 +62,27 @@ function files = plot_block(block, channels, step_size)
         performance = (vertcat(vertcat(all_windows_of_block{:}).hits) + vertcat(vertcat(all_windows_of_block{:}).CRs)) ./ ...
         (vertcat(vertcat(all_windows_of_block{:}).hits) + vertcat(vertcat(all_windows_of_block{:}).CRs) + vertcat(vertcat(all_windows_of_block{:}).misses) + vertcat(vertcat(all_windows_of_block{:}).FAs));
         time = 0:size(all_windows_of_block,1)-1;
-        time = time' * step_size/1000;
+        time = time' * step_size/1000 + window_size/1000/2;
 
-        err = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).error);
-        r_squared = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).r_squared);
+        % Error calculation if needed
+        % err = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).error);
+        % r_squared = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).r_squared);
+        % Outlayer removal if needed
+        % aperiodic_parameters(r_squared > median(r_squared)+1.4826 * 3 * mad(r_squared),:) = nan;
+        % aperiodic_parameters(r_squared < median(r_squared)-1.4826 * 3 * mad(r_squared),:) = nan;
 
         subplot(2,2,i_channel);
         hold on
         yyaxis left
-        plot(time, aperiodic_parameters(:,2))%fillmissing(aperiodic_parameters(:,2),'linear'), 'DisplayName', block_name);
-        ylabel('Aperiodic Exponent')
-        % ylim([1.65 1.7])
+        plot(time, aperiodic_parameters(:,parameter_idx))
+        % if nan values present
+        %fillmissing(aperiodic_parameters(:,2),'linear'), 'DisplayName', block_name);
+        ylabel(['Aperiodic ' parameter])
         yyaxis right
         plot(time, performance);
         ylabel('Performance')
-        % ylim([0 1])
-        title(['Aperiodic offset for channel ' num2str(channels(i_channel))])
+        title(['Aperiodic ' parameter ' for channel ' num2str(channels(i_channel))])
         xlabel('Time in s')
+        xlim([0 max(time)+window_size/1000/2])
     end
 end
