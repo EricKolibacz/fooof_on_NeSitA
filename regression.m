ceb6b8fd7425cfdb58cfdebb154d740cf62d4d28
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-06-24 12:15:36 +0200

    Include crossvalidation meassure and relocate plotting - crossvalidation via crossval - inspration through https://www.mathworks.com/matlabcentral/answers/323449-how-do-i-create-a-cross-validated-linear-regression-model-with-fitlm

--- regression.m
+++ regression.m
@@ -3,6 +3,13 @@ max_shift = max_shift_time/step_size;
 % computation
 linear_models = struct;
 
+
+% Cross validation
+% inspiration from https://www.mathworks.com/matlabcentral/answers/323449-how-do-i-create-a-cross-validated-linear-regression-model-with-fitlm
+% and from crossval function (third option)
+% prediction function given training and testing instances
+fcn = @(XTRAIN,YTRAIN,XTEST) predict(fitlm(XTRAIN,YTRAIN), XTEST);
+
 for shift = -max_shift:1:max_shift
     disp(shift)
     T_shift = table();
@@ -17,18 +24,16 @@ for shift = -max_shift:1:max_shift
         T_shift.Properties.VariableNames{['exponents' num2str(i_channel)]} = ['c' num2str(channels(i_channel)) '_exponent'];
     end
     T_shift.performance = log(2 - T_shift.performance);
-    linear_models.(strrep(['shift' num2str(shift)], '-', 'negative')) = fitlm(T_shift);
+    
+    % Regression on full data
+    linear_models.(strrep(['shift' num2str(shift)], '-', 'negative')).model = fitlm(T_shift);
+    
+    % Cross validation
+    T_array = table2array(T_shift);
+    X = T_array(:,1:8);
+    Y = T_array(:,9);
+
+    % perform cross-validation, and return average MSE across folds
+    mse = crossval('mse', X, Y,'Predfun',fcn,'kfold',5);
+    linear_models.(strrep(['shift' num2str(shift)], '-', 'negative')).rmse = sqrt(mse); 
 end
-
-%% Comparison
-
-
-linear_models_cell = struct2cell(linear_models);
-r_squared_adjusted = nan(length(linear_models_cell),1);
-for linear_model_i = 1:length(linear_models_cell)
-    r_squared_adjusted(linear_model_i,1) = linear_models_cell{linear_model_i}.Rsquared.Adjusted;
-end
-time = -max_shift_time:step_size:max_shift_time;
-plot(time / 1000, r_squared_adjusted)
-xlabel('Shift in s')
-ylabel('R squared adjusted')
