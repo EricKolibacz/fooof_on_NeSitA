2e44c2307307dcde5af512f23bd324873a5b4893
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-07-02 17:17:52 +0200

    Add plotting displaying goodness of fooof fit

--- plotting.m
+++ plotting.m
@@ -11,8 +11,30 @@ for block_i = relevant_blocks_idx
     legend(split(num2str(channels)))
 end
 sgtitle('Xcorr between aperiodic exponent and performance') 
-%% Every block Aperiodic offset
+
+%% Every block R_squared
 figure(2);
+clf;
+for block_i = relevant_blocks_idx
+    subplot(4,4,block_i-relevant_blocks_idx(1)+1)
+    
+    block_name = block_names{block_i};
+    all_windows_of_block = struct2cell(block_results.(block_name));
+    r_squared = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).r_squared);
+    
+    time = 0:size(all_windows_of_block,1)-1;
+    time = time' * step_size/1000 + window_size/1000/2;
+    
+    plot(time,r_squared)
+    title(strrep(block_names(block_i), '_', '-'))
+    xlabel('Time in s')
+    ylabel("Fooof's R Squared")
+    xlim([0 max(time)+window_size/1000/2])
+    legend(split(num2str(channels)))
+end
+sgtitle("Fooof's R squared per window per block") 
+%% Every block Aperiodic offset
+figure(3);
 clf;
 for block_i = relevant_blocks_idx
     subplot(4,4,block_i-relevant_blocks_idx(1)+1)
@@ -25,7 +47,7 @@ for block_i = relevant_blocks_idx
 end
 sgtitle('Xcorr between aperiodic offset and performance') 
 %% Average Rs and ps
-figure(3);
+figure(4);
 subplot(2,2,1)
 plot(ns,permute(nanmean(Rs_ap_exponent,1),[3,2,1]))
 title('Aperiodic exponent')
@@ -55,13 +77,13 @@ legend(split(num2str(channels)))
 
 sgtitle('Xcorr between aperiodic offset and performance average over blocks') 
 %% Plotting aperiodic parameters
-figure(4);
+figure(5);
 clf;
 plot_block(block_results.dist_pred_permanent_2, channels, step_size, window_size, 'exponent', 'performance')
 
 
 %% Plotting regression results
-figure(5);
+figure(6);
 clf;
 
 linear_models_cell = struct2cell(linear_models);
