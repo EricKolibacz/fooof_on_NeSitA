8912ef3f0f59a37fef60c014835bb7423dce7455
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-07-06 12:04:07 +0200

    Improve window field handeling - instead of channels and cluster - now window fields called

--- analysis_blocks.m
+++ analysis_blocks.m
@@ -1,6 +1,6 @@
 %% Parameters
 parent_folder = '/home/eric/Documents/Uni/Master Human Factors/Thesis/Code/data/';
-window_size = 10000;
+window_size = 7500;
 step_size = 1000;
 max_shift_time = 44000; % longest time reasonable for shifting when computing cross correlation
 
@@ -39,6 +39,9 @@ if is_clustered
     amount_per_cluster = [7, 7, 7, 7];
     disp(['A clustered data set was used. The cluster are set to "' strjoin(cluster, ' ') '" with "' num2str(amount_per_cluster) '" as amount of nodes per cluster.'])
     disp('If you like to change this configuration change variables "cluster" and "amount_per_cluster".')
+    window_field_names = strcat('cluster_', cluster);
+else
+	window_field_names = strcat('channel_', strsplit(num2str(channels),' '));  
 end
 
 data_file_splitted = split(data_file{1}, '.');
@@ -97,23 +100,19 @@ end
 
 mode = 'performance';
 relevant_blocks_idx = 8:23;
-Rs_ap_offset = nan(length(relevant_blocks_idx), length(channels), max_shift_time/step_size*2+1);
-ps_ap_offset = nan(length(relevant_blocks_idx), length(channels), max_shift_time/step_size*2+1);
-Rs_ap_exponent = nan(length(relevant_blocks_idx), length(channels), max_shift_time/step_size*2+1);
-ps_ap_exponent = nan(length(relevant_blocks_idx), length(channels), max_shift_time/step_size*2+1);
+Rs_ap_offset = nan(length(relevant_blocks_idx), length(window_field_names), max_shift_time/step_size*2+1);
+ps_ap_offset = nan(length(relevant_blocks_idx), length(window_field_names), max_shift_time/step_size*2+1);
+Rs_ap_exponent = nan(length(relevant_blocks_idx), length(window_field_names), max_shift_time/step_size*2+1);
+ps_ap_exponent = nan(length(relevant_blocks_idx), length(window_field_names), max_shift_time/step_size*2+1);
 for block_name_i = relevant_blocks_idx
     block_name = block_names{block_name_i}
     if isempty(block_results.(block_name))
        continue 
     end
-    for i_channel = 1:length(channels)
-        if is_clustered
-            channel = ['cluster_' cluster{i_channel}];
-        else
-            channel = ['channel_' num2str(channels(i_channel))];
-        end
+    for window_field_name_i = 1:length(window_field_names)
         all_windows_of_block = struct2cell(block_results.(block_name));
-        aperiodic_parameters = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).aperiodic_params);
+        field_name = (window_field_names{window_field_name_i});
+        aperiodic_parameters = vertcat(vertcat(vertcat(all_windows_of_block{:}).(field_name)).aperiodic_params);
         [performance, bias] = get_performance(all_windows_of_block);
         
         if strcmp(mode, 'performance')
@@ -127,19 +126,19 @@ for block_name_i = relevant_blocks_idx
 %             continue
 %         end
     
-        error = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).error);
-        r_squared = vertcat(vertcat(vertcat(all_windows_of_block{:}).(channel)).r_squared);
+        error = vertcat(vertcat(vertcat(all_windows_of_block{:}).(field_name)).error);
+        r_squared = vertcat(vertcat(vertcat(all_windows_of_block{:}).(field_name)).r_squared);
         % median(r_squared)+1.4826 * 3 * mad(r_squared)
         % median(r_squared)-4.4478*mad(r_squared)
         aperiodic_parameters(r_squared > median(r_squared)+1.4826 * 3 * mad(r_squared),:) = nan;
         aperiodic_parameters(r_squared < median(r_squared)-1.4826 * 3 * mad(r_squared),:) = nan;
     
         [~, Rs, ps] = cross_correlation(comparison_parameter, aperiodic_parameters(:,1), max_shift_time/step_size);
-        Rs_ap_offset(block_name_i-relevant_blocks_idx(1)+1,i_channel,:) = Rs;
-        ps_ap_offset(block_name_i-relevant_blocks_idx(1)+1,i_channel,:) = ps;
+        Rs_ap_offset(block_name_i-relevant_blocks_idx(1)+1,window_field_name_i,:) = Rs;
+        ps_ap_offset(block_name_i-relevant_blocks_idx(1)+1,window_field_name_i,:) = ps;
         
         [ns, Rs, ps] = cross_correlation(comparison_parameter, aperiodic_parameters(:,2), max_shift_time/step_size);
-        Rs_ap_exponent(block_name_i-relevant_blocks_idx(1)+1,i_channel,:) = Rs;
-        ps_ap_exponent(block_name_i-relevant_blocks_idx(1)+1,i_channel,:) = ps;
+        Rs_ap_exponent(block_name_i-relevant_blocks_idx(1)+1,window_field_name_i,:) = Rs;
+        ps_ap_exponent(block_name_i-relevant_blocks_idx(1)+1,window_field_name_i,:) = ps;
     end
 end
