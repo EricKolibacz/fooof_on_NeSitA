006b43ffded076b659855ff94d059fffbb8ba2f8
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-08-20 10:23:38 +0200

    Remove print out (variable i)

--- change_block_name.m
+++ change_block_name.m
@@ -4,7 +4,6 @@ function [block_name] = change_block_name(block_name)
     level_short_names = {'D+', 'D-', 'P+', 'P-', 'V+', 'V-'};
     
     for i=length(level_names):-1:1
-        i
         block_name = replace(block_name, level_names{i}, level_short_names{i});
     end
     block_name = replace(block_name, '_', '/');
