5020a928b6939b0c7e45e4e9468bd94fa7e9f483
eric.kolibacz <kolibacz@campus.tu-berlin.de>
2021-10-19 14:40:16 +0200

    Add one further window size to analysis

--- creating_block_analysis.m
+++ creating_block_analysis.m
@@ -3,7 +3,7 @@ set_parameters;
 datafiles_folder = '/media/eric/External/Daten/Data4Pan/Data4Pan';
 persons = {'s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21', 's22', 's23', 's24', 's25', 's26', 's27', 's28', 's29', 's30', 's31', 's32'};
 %persons = {'s22', 's23', 's24', 's25', 's26', 's27', 's28', 's29', 's30', 's31', 's32'};
-window_sizes = [7500, 10000, 15000];
+window_sizes = [5000, 7500, 10000, 15000];
 step_sizes = [1000];
 
 %% Computation
